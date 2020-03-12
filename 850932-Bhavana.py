d1ce5b1a2ea9ad8320f8a9a839e2327f9282e508
Bhavana21598 <61181600+Bhavana21598@users.noreply.github.com>
2020-03-12 09:12:20 +0530

    Add files via upload

--- 850932-Bhavana.py
+++ 850932-Bhavana.py
@@ -1,16 +1,14 @@
-#List all running instances in AWS.
-import boto.ec2
+#Copying Buckets
+import boto3
+s3_client = boto3.client('s3')
 
-import sys
-
-regions = boto.ec2.regions()
-for region in regions:
-    try:
-        ec2conn = boto.ec2.connect_to_region(region.name);
-        reservations = ec2conn.get_all_reservations();
-        for reservation in reservations:
-                for instance in reservation.instances:
-                    print instance.image_id + " " + str(instance.tags)
-
-    except:
-        print(sys.exc_info());
+def copy_to_bucket(bucket_from_name, bucket_to_name, file_name):
+    copy_source = {
+        'Bucket': bucket_from_name,
+        'Key': file_name
+    }
+    s3_resource.Object(bucket_to_name, file_name).copy(copy_source)
+#Removing the first bucket
+s3_resource.Bucket(first_bucket_name).delete()
+#Client version to remove the second bucket
+s3_resource.meta.client.delete_bucket(Bucket=second_bucket_name)
